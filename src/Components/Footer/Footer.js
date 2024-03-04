6292b652accc587d47df606ef25bac62b9f7c411
Aditya Singh <singhadi280303@gmail.com>
2024-03-04 21:49:29 +0530

    m

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,61 @@
 import React from "react";
-
+import "./Footer.css";
 function Footer() {
   return (
-    <div className="conatiner-fluid bg-dark mt-5">
-      <footer className="text-white text-center p-2">@2024</footer>
+    <div className="conatiner-fluid bg-dark mt-5 foot">
+      <footer className="footer">
+        <div className="container">
+          <div className="footer-content">
+            <div className="footer-logo">OSCILLATIONS'24</div>
+            <div className="footer-links">
+              <ul className="footer-menu">
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Tracks</a>
+                </li>
+                <li>
+                  <a href="#">Rules</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-social">
+              <ul className="social-icons">
+                <li>
+                  <a href="#">
+                    <i className="fab fa-facebook-f" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fab fa-twitter" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fab fa-instagram" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fab fa-linkedin-in" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <p>© 2024 Oscillations. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
