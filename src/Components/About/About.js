b9e1a6021a1eedd27d7bdc4658c4382cc073513a
Aditya Singh <singhadi280303@gmail.com>
2024-03-21 20:43:46 +0530

    images

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../About/About.css";
 import png1 from "../About/WhatsApp Image 2024-03-15 at 20.53.53_8ff11c11.jpg";
-import png2 from "../About/csi.png"
-import png3 from "../About/ieee.png"
-import png4 from "../About/iete.png"
-import png5 from "../About/isa.png"
-import png6 from "../About/ishrae.png"
-import png7 from "../About/igbc.png"
-import png8 from "../About/vmea.png"
-import png9 from "../About/image.png"
+import png2 from "../About/csi.png";
+import png3 from "../About/ieee.png";
+import png4 from "../About/iete.png";
+import png5 from "../About/isa.png";
+import png6 from "../About/ishrae.png";
+import png7 from "../About/igbc.png";
+import png8 from "../About/vmea.png";
+import png9 from "../About/image.png";
 function About() {
   return (
     <div className="container-fluid mt-5 pt-3  " id="about">
@@ -20,10 +20,10 @@ function About() {
           Technical Fest organized by Vidyavardhini's College of Engineering and
           Technology. VNPS is realized by the combined efforts of various
           departments' student committees: IEEE, ISA, CSI, IETE, VMEA, ISHRAE,
-          IGBC and IIC. They work in sync to provide a platform to the students with
-          different project ideas to come together and present their ideas and
-          innovations. VNPS aims to inspire students to think, motivate and
-          innovate.
+          IGBC, NSDC and IIC. They work in sync to provide a platform to the
+          students with different project ideas to come together and present
+          their ideas and innovations. VNPS aims to inspire students to think,
+          motivate and innovate.
         </p>
         <div className="container mt-5">
           <div className="row justify-content-center p-0 gap-2 m-auto">
@@ -95,7 +95,11 @@ function About() {
               <img
                 src={png9}
                 className="img rounded bg-white"
-                style={{ width: "12rem",paddingTop:'11px',paddingBottom:'11px' }}
+                style={{
+                  width: "12rem",
+                  paddingTop: "11px",
+                  paddingBottom: "11px",
+                }}
                 alt=""
               />
             </div>
